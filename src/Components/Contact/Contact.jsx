eb60ce2dca1fc5b408c9fa9767ddd8a81fff2c67
Rakib Sheikh <sheikhrakib883@gmail.com>
2024-02-24 23:16:10 +0600

    design

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -12,8 +12,11 @@ const Contact = () => {
     const [usergmail,setUsergmail] = useState();
     const [usersub,setuseSubject] = useState();
     const [usertext , setUsertext] =  useState();
+    const [usererror , setError] = useState(false);
     const submitHandeler = ()=>{
-        console.log(uservalue)
+        if(userphone === ""){
+            console.log("inter your value")
+        }
     }
     return (
         <div className="main-area ">
@@ -60,6 +63,8 @@ const Contact = () => {
                             <div className="phone-and-name-area flex justify-between gap-4">
                                 <div className="phone w-full">
                                     <p className="text-[16px] uppercase text-[#C4CFDE]">Inter your name</p>
+                                    
+                                    
                                  <input onChange={(e) =>setUservalue(e.target.value)} value={uservalue}  className="bg-[#191B1E] border-2  mt-1  text-[#C4CFDE] rounded-sm font-mainFont outline-none border-[black] focus-within:border-[#E11D48] duration-300 py-3 w-full px-2" placeholder="Inter your name" type="text" />
                                 </div>
                                 <div className="name-area w-full">
@@ -83,7 +88,7 @@ const Contact = () => {
                                     <p className="text-[16px] uppercase text-[#C4CFDE]">Inter your message</p>
                                     <textarea onChange={(e) =>setUsertext(e.target.value)} value={usertext} className="w-full bg-[#191B1E] border-2 border-[black] focus-within:border-[#E11D48] mt-1  text-[#C4CFDE] rounded-sm font-mainFont outline-none  duration-300 py-3 px-2" placeholder="Inter your subject" name="" id="" cols="" rows="10"></textarea>
                                 </div>
-                                <div onClick={submitHandeler} className="text-center py-2 cursor-pointer mt-5 uppercase font-mainFont bg-[green]">Send message</div>
+                                <button onClick={submitHandeler} className="text-center border border-[black] duration-300 w-full text-[white] focus-within:border-[#E11D48] py-2 cursor-pointer mt-5 uppercase font-mainFont bg-[#212428]">Send message</button>
                             </div>
                             </div>
                         </div>
